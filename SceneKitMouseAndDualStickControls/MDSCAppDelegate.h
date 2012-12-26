61fde1f210c6b0c0ab8eb606ac9ab749cf3ea0f1
Steven Troughton-Smith <steventroughtonsmith@gmail.com>
2012-12-26 14:40:58 +0000

    Initial Commit

---
+++ SceneKitMouseAndDualStickControls/MDSCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MDSCAppDelegate.h
+//  SceneKitMouseAndDualStickControls
+//
+//  Created by Steven Troughton-Smith on 26/12/2012.
+//  Copyright (c) 2012 High Caffeine Content. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MDSCAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ SceneKitMouseAndDualStickControls/MDSCAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  MDSCAppDelegate.m
+//  SceneKitMouseAndDualStickControls
+//
+//  Created by Steven Troughton-Smith on 26/12/2012.
+//  Copyright (c) 2012 High Caffeine Content. All rights reserved.
+//
+
+#import "MDSCAppDelegate.h"
+
+@implementation MDSCAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
